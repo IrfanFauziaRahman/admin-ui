5852af674518a08499d5fe217ee26db74dedd8f0
IRFAN FAUZIA RAHMAN <111202113631@mhs.dinus.ac.id>
2024-05-03 14:10:04 +0700

    Last Part Before UTS

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formsource";
 
 function App() {
   return (
@@ -18,13 +19,19 @@ function App() {
             <Route path="users">
               <Route index element={<List />}></Route>
               <Route path=":userId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
+              <Route 
+              path="new" 
+              element={<New inputs={userInputs} title="Add New User"/>}
+              />
             </Route>
 
             <Route path="products">
               <Route index element={<List />}></Route>
               <Route path=":productsId" element={<Single />}></Route>
-              <Route path="new" element={<New />}></Route>
+              <Route 
+              path="new" 
+              element={<New inputs={productInputs} title="Add new Product"/>}
+              />
             </Route>
           </Route>
         </Routes>

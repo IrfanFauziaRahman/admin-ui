0a13ccc4df8923ef40deb5efa56a81147534694e
IRFAN FAUZIA RAHMAN <111202113631@mhs.dinus.ac.id>
2024-06-13 00:40:04 +0700

    ubah teks di app

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+const {dispatch} = useContext(DarkModeContext);
+
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -12,14 +17,11 @@ const Navbar = () => {
         </div>
         <div className="items">
           <div className="item">
-            <DarkModeOutlinedIcon className="icon" />
+            <DarkModeOutlinedIcon className="icon"
+             onClick={() => dispatch({type: "TOGGLE"})} />
           </div>
           <div className="item">
-            <img
-              src="https://i.ibb.co/rHpSNGH/marcell.jpg"
-              alt=""
-              className="avatar"
-            />
+              <AccountCircleIcon className="icon"/>
           </div>
         </div>
       </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import "./sidebar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import InventoryIcon from '@mui/icons-material/Inventory';
@@ -7,8 +7,30 @@ import StoreIcon from '@mui/icons-material/Store';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LogoutIcon from '@mui/icons-material/Logout';
 import CategoryIcon from '@mui/icons-material/Category';
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+import { signOut } from "firebase/auth";
+import {auth} from "../../firebase";
+import { AuthContext } from "../../context/AuthContext";
 
 const Sidebar = () => {
+  const {dispatch} = useContext(DarkModeContext);
+
+  const {dispatch: authDispatch} = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () =>{
+    signOut(auth)
+    .then(()=>{
+      authDispatch({ type: "LOGOUT"});
+      navigate("/login");
+    })
+    .catch((error)=>{
+      console.error("Logout error:", error);
+    });
+  };
+    
+
     return (
         <div className="sidebar">
   <div className="top">
@@ -59,8 +81,8 @@ const Sidebar = () => {
     </ul>
   </div>
   <div className="bottom">
-    <div className="colorOption"></div>
-    <div className="colorOption"></div>
+    <div className="colorOption" onClick={() => dispatch({type: "LIGHT"})}></div>
+    <div className="colorOption" onClick={() => dispatch({type: "DARK"})}></div>
   </div>
 </div>
     );
